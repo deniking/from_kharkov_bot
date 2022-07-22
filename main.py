88199e087a0aaa1bc17b8beb9a204ba1cf4fd56b
DENIS <denikingya@gmail.com>
2022-07-22 18:05:35 +0300

    add file

---
+++ main.py
@@ -0,0 +1,28 @@
+# bot
+import telebot
+import random
+
+bot = telebot.TeleBot('5507838809:AAHtBfJfWZ1AvNqG5X7ua2Fw-0XriEaHvqg')
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    msg=message.text
+    msg=msg.lower()
+    if (u'привет' in msg):
+        privet = ["Привет лапух", "Приветик", "Че хотел?", "Ну и дальше что?", "Привед-ВЕДМЕД 😂"]
+        bot.send_message(message.chat.id, random.choice(privet))
+    elif (u'как дела' in msg):
+        privet = ["Дела у прокурора", "Дела делишки", "Че хотел то?", "Денег не дам", "Зашибись😂"]
+        bot.send_message(message.chat.id, random.choice(privet))
+    elif (u'что делаешь' in msg):
+        privet = ["сухари сушу", "Дела делаю", "Бухаю😂?", "НИХ@Я", "читаю что тут пишут"]
+        bot.send_message(message.chat.id, random.choice(privet))
+    elif (u'прилет' in msg):
+        privet = ["ТСССС....🤨", "ох уж эти орки", "про место молчим!", "бух-бах", "Бля, достали 😒"]
+        bot.send_message(message.chat.id, random.choice(privet))
+    elif (u'доброе утро' in msg):
+        privet = ["утро добрым не бывает 😂", "добрее видали ", "и вам не хворать", "✌️", "че не спишь?"]
+        bot.send_message(message.chat.id, random.choice(privet))
+
+bot.polling()
